b1a2b750763795f7bb1a746949ec161698e4f9b8
diniljose <shilpa.venkatesh@fedo.ai>
2022-09-08 12:52:51 +0530

    deploy

--- src/app/constants/index.ts
+++ src/app/constants/index.ts
@@ -2,4 +2,4 @@ export const ADMIN_URL = "/sales/api/v1/";
 
 
 export const API_URL = ADMIN_URL+"vitals/";
-export const API_ADMIN_URL = ADMIN_URL+"/admin/";
+export const API_ADMIN_URL = ADMIN_URL+"admin/";
