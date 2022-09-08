be341855b0c6d7ed66d673f1521c11206575f2db
diniljose <shilpa.venkatesh@fedo.ai>
2022-09-08 13:23:33 +0530

    deploy

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -73,7 +73,7 @@ export class LoginComponent implements OnInit {
         },
         error: (error: string) => {
           console.log('asdf',error)
-          this.error = 'invalid username or password';
+          this.error = error;
         }});
 
 
