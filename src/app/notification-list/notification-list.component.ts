98f94fa997cfc4b3e044e9dff3a2d66d4ab61739
mohammedamraz <mohammed.amraz@fedo.ai>
2023-01-05 18:54:35 +0530

    fixed pagination error and mark all as read

--- src/app/notification-list/notification-list.component.ts
+++ src/app/notification-list/notification-list.component.ts
@@ -31,6 +31,8 @@ export class NotificationListComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    // and one more thing what we can do is give the same thing what we have in the other list but i think it is not needed since we are having the same thing in all the places, 
+    // so as of now we dont need such things i guess
 // here the is_read boolean should be given like true or false
   this.adminService.fetchAllUnreadOrgByPage(this.pagenumber,this.entries,ACTIVE[this.readStatusValue]).subscribe((doc:any)=>{
     console.log("orglist",doc);
@@ -85,7 +87,7 @@ export class NotificationListComponent implements OnInit {
   markAllAsRead(){
     this.adminService.fetchAllUnreadOrgByPage(1,1000000,false).subscribe((doc:any)=>{
       console.log("orglist",doc);
-      doc.map((doc:any)=>{
+      doc.data.map((doc:any)=>{
         console.log("doc",doc)
         // const v = Object.assign(doc, {vitalsTest:2})
         this.updateReadStatus(doc.id)
